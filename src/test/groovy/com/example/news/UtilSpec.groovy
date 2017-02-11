75bd78af1da5235952b389410cffc6b12a5e2df2
kenix <zhongda.zhao@gmail.com>
2017-02-11 13:03:34 +0100

    refactor tests, add gradle test event logging output

--- src/test/groovy/com/example/news/UtilSpec.groovy
+++ src/test/groovy/com/example/news/UtilSpec.groovy
@@ -12,11 +12,13 @@ import java.nio.ByteBuffer
 class UtilSpec extends Specification {
 
     def 'byte to unsigned int'() {
-        expect:
+        expect: 'correct conversion between unsigned byte and int'
         Util.unsignedToInt(0 as byte) == 0 as int
         Util.unsignedToInt(1 as byte) == 1 as int
         Util.unsignedToInt(-1 as byte) == 255 as int
+        Util.unsignedToInt(255 as byte) == 255 as int
         Util.unsignedToInt(127 as byte) == 127 as int
+        Util.unsignedToInt(130 as byte) == 130 as int
     }
 
     def 'news encoding and parsing'() {
--- src/test/groovy/com/example/news/analyser/AnalyserSpec.groovy
+++ src/test/groovy/com/example/news/analyser/AnalyserSpec.groovy
@@ -12,11 +12,11 @@ import java.util.concurrent.PriorityBlockingQueue
 class AnalyserSpec extends Specification {
 
     @Unroll
-    def '#news is positive #status'() {
-        given:
+    def '#news positivity #status'() {
+        given: 'an analyser'
         def analyser = new Analyser()
 
-        expect:
+        expect: 'the analyser can decide if a news if positive'
         analyser.isPositiveNews(news) == status
 
         where:
@@ -29,21 +29,22 @@ class AnalyserSpec extends Specification {
     }
 
     def 'sort news'() {
-        given:
+        given: 'an analyser'
         def analyser = new Analyser()
-        and:
+        and: 'a priority queue'
         def prioQueue = new PriorityBlockingQueue<News>()
-        and:
+
+        when: 'putting 5 news into the queue'
         prioQueue.offer(new News(1, ['down', 'down', 'down']))
         prioQueue.offer(new News(2, ['up', 'down', 'down']))
         prioQueue.offer(new News(3, ['up', 'up', 'down']))
         prioQueue.offer(new News(4, ['up', 'up', 'up']))
         prioQueue.offer(new News(5, ['up', 'up', 'down', 'down']))
-
-        when:
+        and: 'letting the analyser to sort those news in the queue'
         def sortedNews = analyser.sortNews(prioQueue)
-        then:
+        then: 'getting 5 news back'
         sortedNews.length == 5
+        and: 'they are in the expected order: prio desc'
         sortedNews as List == [
                 new News(5, ['up', 'up', 'down', 'down']),
                 new News(4, ['up', 'up', 'up']),
@@ -54,29 +55,29 @@ class AnalyserSpec extends Specification {
     }
 
     def 'inspect news'() {
-        given:
+        given: 'an analyser'
         def analyser = new Analyser()
 
-        when:
+        when: 'analysing 5 news, of which 2 are positive'
         analyser.analyseNews(new News(1, ['down', 'down', 'down']))
         analyser.analyseNews(new News(2, ['up', 'down', 'down']))
         analyser.analyseNews(new News(3, ['up', 'up', 'down']))
         analyser.analyseNews(new News(4, ['up', 'up', 'up']))
         analyser.analyseNews(new News(5, ['up', 'up', 'down', 'down']))
-        then:
+        then: 'the analyser counted the positive news correctly'
         analyser.positiveNewsCount.get() == 2L
-        and:
+        and: 'ignored negative news, i.e. only queued positive news'
         analyser.prioQueueRef.get().size() == 2
-        and:
+        and: 'the prio queue for exchange between inspecting and analysing threads is empty'
         !analyser.prioQueueForExchange.size()
 
-        when:
+        when: 'the analyser inspects the statistics'
         analyser.inspect()
-        then:
+        then: "the analyser's counter for positive news is reset"
         !analyser.positiveNewsCount.get()
-        and:
+        and: 'the prio queue is empty after inspecting without parallel analysing'
         !analyser.prioQueueRef.get().size()
-        and:
+        and: 'the prio queue for exchange between inspecting and analysing threads is reset'
         !analyser.prioQueueForExchange.size()
     }
 }
